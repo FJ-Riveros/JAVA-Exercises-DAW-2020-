99dcbe39906a027164ea7bd5fa9a3fdbc419ab48
FJ-Riveros <fran.etnies@gmail.com>
2020-10-03 23:12:52 +0200

    Actualización

---
+++ Capitulo01/C01Ejercicio09.java
@@ -0,0 +1,25 @@
+public class C01Ejercicio09{
+  public static void main ( String [] args){
+  System.out.println("                                                                  "); 
+  System.out.println("                                                                  "); 
+  System.out.println("                           ██████                                 ");
+  System.out.println("                           █    █                                 ");
+  System.out.println("                           █    █                                 ");
+  System.out.println("                           ██████                                 ");
+  System.out.println("                   ▄         ██         ▄                         ");
+  System.out.println("                    ████████████████████                          ");
+  System.out.println("                             ██                                   ");
+  System.out.println("                             ██                                   "); 
+  System.out.println("                             ██                                   ");
+  System.out.println("                             ██                                   ");
+  System.out.println("                             ██                                   ");
+  System.out.println("     ██                      ██                      ██           ");
+  System.out.println("      ██                     ██                     ██            ");
+  System.out.println("       ███                   ██                   ███             ");
+  System.out.println("         ███                 ██                 ███               ");
+  System.out.println("           ██████████████████████████████████████                 "); 
+  System.out.println("              ████████████████████████████████                    ");
+  System.out.println("                                                                  ");
+  System.out.println("                                                                  ");
+  }
+}
