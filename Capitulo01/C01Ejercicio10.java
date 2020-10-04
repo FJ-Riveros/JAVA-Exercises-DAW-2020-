d9d44f695fa2f4378b8b524ef5c136f08baa042c
FJ-Riveros <fran.etnies@gmail.com>
2020-10-04 12:06:35 +0200

    Actualización

--- Capitulo01/C01Ejercicio10.java
+++ Capitulo01/C01Ejercicio10.java
@@ -1,25 +1,25 @@
 public class C01Ejercicio09{
   public static void main ( String [] args){
-  System.out.println("                                                                     "); 
+  System.out.println("\033[94m                                                                    "); 
   System.out.println("                                                                     "); 
   System.out.println("                              ██████                                 ");
   System.out.println("                              █    █                                 ");
   System.out.println("                              █    █                                 ");
   System.out.println("                              ██████                                 ");
-  System.out.println("                      ▄         ██         ▄                         ");
+  System.out.println("\033[34m                      ▄         ██         ▄                         ");
   System.out.println("                       ████████████████████                          ");
   System.out.println("                                ██                                   ");
   System.out.println("                                ██                                   "); 
   System.out.println("                                ██                                   ");
   System.out.println("                                ██                                   ");
   System.out.println("                                ██                                   ");
-  System.out.println("      ██                        ██                        ██         ");
+  System.out.println("\033[96m      ██                        ██                        ██         ");
   System.out.println("        ██                      ██                      ██           ");
   System.out.println("          ███                   ██                   ███             ");
   System.out.println("            ████                ██                ████               ");
   System.out.println("              ██████            ██            ██████                 "); 
   System.out.println("                ███████         ██         ███████                   ");
-  System.out.println("                  ████████      ██      ████████                     ");
+  System.out.println("\033[92m                  ████████      ██      ████████                     ");
   System.out.println("                     ████████████████████████                        ");
   System.out.println("                         ████████████████                            "); 
   }
