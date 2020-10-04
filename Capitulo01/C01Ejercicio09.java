3c01057b19ebcf62266df969d73c5926d733eab6
FJ-Riveros <fran.etnies@gmail.com>
2020-10-04 11:20:28 +0200

    Actualizaciónk

--- Capitulo01/C01Ejercicio09.java
+++ Capitulo01/C01Ejercicio09.java
@@ -1,25 +1,26 @@
 public class C01Ejercicio09{
   public static void main ( String [] args){
-  System.out.println("                                                                  "); 
-  System.out.println("                                                                  "); 
-  System.out.println("                           ██████                                 ");
-  System.out.println("                           █    █                                 ");
-  System.out.println("                           █    █                                 ");
-  System.out.println("                           ██████                                 ");
-  System.out.println("                   ▄         ██         ▄                         ");
-  System.out.println("                    ████████████████████                          ");
-  System.out.println("                             ██                                   ");
-  System.out.println("                             ██                                   "); 
-  System.out.println("                             ██                                   ");
-  System.out.println("                             ██                                   ");
-  System.out.println("                             ██                                   ");
-  System.out.println("     ██                      ██                      ██           ");
-  System.out.println("      ██                     ██                     ██            ");
-  System.out.println("       ███                   ██                   ███             ");
-  System.out.println("         ███                 ██                 ███               ");
-  System.out.println("           ██████████████████████████████████████                 "); 
-  System.out.println("              ████████████████████████████████                    ");
-  System.out.println("                                                                  ");
-  System.out.println("                                                                  ");
+  System.out.println("                                                                     "); 
+  System.out.println("                                                                     "); 
+  System.out.println("                              ██████                                 ");
+  System.out.println("                              █    █                                 ");
+  System.out.println("                              █    █                                 ");
+  System.out.println("                              ██████                                 ");
+  System.out.println("                      ▄         ██         ▄                         ");
+  System.out.println("                       ████████████████████                          ");
+  System.out.println("                                ██                                   ");
+  System.out.println("                                ██                                   "); 
+  System.out.println("                                ██                                   ");
+  System.out.println("                                ██                                   ");
+  System.out.println("                                ██                                   ");
+  System.out.println("      ██                        ██                        ██         ");
+  System.out.println("        ██                      ██                      ██           ");
+  System.out.println("          ███                   ██                   ███             ");
+  System.out.println("            ████                ██                ████               ");
+  System.out.println("              ██████            ██            ██████                 "); 
+  System.out.println("                ███████         ██         ███████                   ");
+  System.out.println("                  ████████      ██      ████████                     ");
+  System.out.println("                     ████████████████████████                        ");
+  System.out.println("                         ████████████████                                             "); 
   }
 }
