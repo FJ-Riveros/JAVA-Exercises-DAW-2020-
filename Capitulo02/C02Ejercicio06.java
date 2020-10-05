a1a4b30fc62d90e4378143374c7a31c54d939507
FJ-Riveros <fran.etnies@gmail.com>
2020-10-05 18:31:21 +0200

    Actualización

--- Capitulo02/C02Ejercicio06.java
+++ Capitulo02/C02Ejercicio06.java
@@ -1,8 +1,8 @@
 public class C02Ejercicio06 {
   public static void main ( String [] args ){
-  System.out.println("Esto es un convertidor de pesetas a euros");
-  double pesetas = 166.386;
-  double resultado = pesetas / pesetas;
-  System.out.println("166.386 pesetas equivalen a: " + resultado + " euros");
+  System.out.println("Este programa calcula el total de una factura a partir de la base imponible");
+  float imponible = 100;
+  double resultado = imponible * 1.21;
+  System.out.println("Para una base imponible de 100 euros el total sería " + resultado + " euros");
   }
 }
