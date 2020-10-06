bc20ca74f3b1a134d73df853ba0c4eeec5a38a25
FJ-Riveros <fran.etnies@gmail.com>
2020-10-06 12:55:40 +0200

    Actualización

--- EjerciciosExamenes/Control01/Examen01Ejercicio02.java
+++ EjerciciosExamenes/Control01/Examen01Ejercicio02.java
@@ -1,13 +1,21 @@
 public class Examen01Ejercicio02 {
   public static void main ( String [] args ) {
   int h;
+  String orientacion;
   System.out.println("Este programa pinta piramides");
   do{
-  System.out.println("Introduzca la altura (1-10): ");
+  System.out.print("Introduzca la altura (1-10): ");
   h = Integer.parseInt(System.console().readLine());
   System.out.println(h);
   }
   while((h > 10) || (h < 1));
   
-  }
+  do{
+    System.out.println("Introduzca la orientación: izquierda, derecha,
+    arriba, abajo");
+    orientacion = System.console().readLine();
+    System.out.println(orientacion);
+  }while(orientacion != "izquierda" || orientacion != "derecha" || orientacion
+  != "arriba" || orientacion != "abajo");
+}
 }
