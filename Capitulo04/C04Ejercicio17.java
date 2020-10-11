5b613d05e5b6caf19b8b5eab98a55f49ba0b6710
FJ-Riveros <fran.etnies@gmail.com>
2020-10-11 13:02:40 +0200

    Actualización

--- Capitulo04/C04Ejercicio17.java
+++ Capitulo04/C04Ejercicio17.java
@@ -1,9 +1,9 @@
 public class C04Ejercicio17{
   public static void main( String [] args){
-    String num; 
+    int num; 
     System.out.println("Este programa dice cuál es la última cifra de un número entero");
     System.out.println("Introduce el número entero deseado");
-    num = System.console().readLine();
-    System.out.println(num.charAt(num.length() - 1));
+    num = Integer.parseInt(System.console().readLine());
+    System.out.println("La última cifra introducida es: " + (num % 10));
   }
 }
