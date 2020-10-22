199f2de1637b0b541b27e64086d239b0c85cdd76
FJ-Riveros <fran.etnies@gmail.com>
2020-10-22 19:03:35 +0200

    Actualización

--- EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa10.java
+++ EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa10.java
@@ -2,17 +2,11 @@ public class EjercicioSorpresa10 {
   public static void main (String [] args){
     System.out.println("Introduzca la altura deseada");
     int num = Integer.parseInt(System.console().readLine());
-    int aux = num - 1;
-    for( int i = 1; i <= num; i++){
-      for( int j = num; j > 0; j--){
-        System.out.print(" ");
+    for( int i = 1; i <= num ; i++){
+      for( int j = 1; j <= ((i * 2) - 1); j++){
+        System.out.print(j + " ");
       }
-      for( int x = 1; x <= i; x++){
-        
-          System.out.print(i);
-        
-      } 
-     System.out.println(); 
+      System.out.println();
     } 
   }
 } 
