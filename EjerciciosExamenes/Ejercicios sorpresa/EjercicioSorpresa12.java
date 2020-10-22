8aeaf229a7bade9cce7b1372c7b32bc97c66b91f
FJ-Riveros <fran.etnies@gmail.com>
2020-10-22 20:04:55 +0200

    Actualización

--- EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa12.java
+++ EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa12.java
@@ -1,17 +1,21 @@
-public class EjercicioSorpresa11{
+public class EjercicioSorpresa12 {
   public static void main (String [] args){
     System.out.println("Introduzca el número de filas que desea");
     int num = Integer.parseInt(System.console().readLine());
+    int k;
     for( int i = 1; i <= num; i++){
-      for( int k = 0; k < (num - i); k++){
+      for( int j = 0; j < ( num - i); j++){
         System.out.print(" ");
       }
-      for( int j = 1; j <= (( i * 2 ) - 1); j++){
-        if( j >= 10){
-          System.out.print( j % 10);
+      for( k = 1; k <= i; k++){
+        if( k >= 10 ){
+          System.out.print(k % 10);
         }else{
-          System.out.print(j);
+          System.out.print(k);
         }
+      }
+      for( int h = k - 2; h >= 1; h--){
+        System.out.print(h);
       }
       System.out.println();
     }
