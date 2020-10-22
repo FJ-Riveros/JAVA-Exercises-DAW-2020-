1bd7c7d167c1ba10fd8b8142468540647f4b6bf9
FJ-Riveros <fran.etnies@gmail.com>
2020-10-22 10:39:28 +0200

    Actualización

--- EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa9.java
+++ EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa9.java
@@ -4,10 +4,12 @@ public class EjercicioSorpresa9{
     System.out.println("Introduzca el número de filas que desee");
     int num = Integer.parseInt(System.console().readLine());
     for( int i = 1; i <= (num * 2); i++){
-      concat += i + " ";
-      System.out.println(concat);
-      i++;
-      concat += i + " ";
+      if( i % 2 != 0 ){
+        concat += i + " ";
+        System.out.println(concat);
+      }else{
+        concat += i + " ";
+      }
     }
   }
 }
