095efb1f602a9c1229d7f01609eb9d4cbca57bed
FJ-Riveros <fran.etnies@gmail.com>
2020-10-26 18:54:05 +0100

    Actualización

--- Capitulo05/C05Ejercicio24.java
+++ Capitulo05/C05Ejercicio24.java
@@ -5,15 +5,15 @@ public class C05Ejercicio24 {
   public static void main (String [] args){
     System.out.println("Introduzca la altura");
     int h = Integer.parseInt(System.console().readLine());
-    int k;
+    
     for( int i = 1; i <= h; i++){
-      for( int j = 0; j <= h - i; j++){
+      for( int j = 1; j <= h - i; j++){
         System.out.print(" ");
       }
-      for(  k = 1; k <= i; k++){
-        System.out.print(i);
+      for(int k = 1; k <= i; k++){
+        System.out.print(k);
       }
-      for( int g = k - 1; g >= 1; g--){
+      for( int g = i - 1; g >= 1; g--){
         System.out.print(g);
       }
       System.out.println();
