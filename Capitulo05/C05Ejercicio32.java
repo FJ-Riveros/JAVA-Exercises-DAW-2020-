7e8d9f04d2605c09edf891ec3613184737d532e6
FJ-Riveros <fran.etnies@gmail.com>
2020-10-29 11:07:58 +0100

    Actualización

---
+++ Capitulo05/C05Ejercicio32.java
@@ -0,0 +1,7 @@
+public class C05Ejercicio32 {
+  public static void main (String [] args) {
+    System.out.print("Introduzca el número deseado: ");
+    int num = Integer.parseInt(System.console().readLine());
+    
+  }
+}
