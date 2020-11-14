fce21c656205c4a4ffd111cd57ba5723dbd7b5b1
FJ-Riveros <fran.etnies@gmail.com>
2020-11-14 16:16:07 +0100

    Actualización

--- Capitulo06/C06Ejercicio20.java
+++ Capitulo06/C06Ejercicio20.java
@@ -1,21 +1,20 @@
 public class C06Ejercicio20 {
   public static void main (String [] args){
-    //NO FUNCIONA
     System.out.println("Introduzca la capacidad de la cuba en litros: ");
     int capacity = Integer.parseInt(System.console().readLine());
     int liters = (int)(Math.random() * (capacity + 1));
-    for( int i = 0; i <= capacity; i++){
+    for( int i = 0; i <= capacity ; i++){
       for( int j = 0; j < 6; j++){
-        if( i == capacity){
+        if( i == capacity  ){
           System.out.print("*");
-        }else if ( liters <= (capacity - i) ){
+        }else{
           if( j == 0 || j == 5 ){
             System.out.print("*");
-          }else{
+          }else if( liters > (capacity - i) - 1 ){
             System.out.print("=");
+          }else{
+            System.out.print(" ");
           }
-        }else{
-          System.out.print(" ");
         }
       }
       System.out.println();
