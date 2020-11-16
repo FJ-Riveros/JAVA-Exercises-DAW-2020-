35cd50d1a2d4ef9c197fa6825675237892bc9571
FJ-Riveros <fran.etnies@gmail.com>
2020-11-16 18:42:50 +0100

    Actualización

---
+++ Capitulo06/C06Ejercicio30.java
@@ -0,0 +1,9 @@
+public class C06Ejercicio30 {
+  public static void main (String [] args){
+    System.out.print("Introduzca la altura de la pecera (como mínimo 4): ");
+    int h = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca el ancho de la pecera (como mínimo 4): ");
+    int w = Integer.parseInt(System.console().readLine());
+    
+  }
+}
