6cf05da0afd0374f1ab3470ccb5a7dce86452b8b
FJ-Riveros <fran.etnies@gmail.com>
2020-11-23 09:02:36 +0100

    Actualización

--- Capitulo 07/C07Ejercicio19.java
+++ Capitulo 07/C07Ejercicio19.java
@@ -4,11 +4,28 @@ public class C07Ejercicio19 {
     for(int i = 0; i < 12; i++){
       original[i] = (int)(Math.random() * 201);
     }
-    System.out.println("Índice   ");
+    System.out.print("Índice   ");
     for( int i = 0; i < 12; i++){
       System.out.printf("%4d" , i);
     }
-    System.out.println("Valor    ");
+    System.out.print("\nValor    ");
+    for( int n: original ){
+      System.out.printf("%4d", n);
+    }
+    System.out.println();
+    System.out.print("Introduzca el número que quiera insertar: ");
+    int insert = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca la posición donde lo quiera insertar(0 - 11): ");
+    int pos = Integer.parseInt(System.console().readLine());
+    for( int i = 11; i > pos; i-- ){
+      original[i] = original[ i - 1];
+    }
+    original[pos] = insert;
+    System.out.print("Índice   ");
+    for( int i = 0; i < 12; i++){
+      System.out.printf("%4d" , i);
+    }
+    System.out.print("\nValor    ");
     for( int n: original ){
       System.out.printf("%4d", n);
     }
