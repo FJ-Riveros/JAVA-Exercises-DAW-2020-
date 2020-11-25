000e9c00e056014ea9e61d5c3295619b9534be76
FJ-Riveros <fran.etnies@gmail.com>
2020-11-25 10:20:07 +0100

    Actualización

--- Capitulo 07/C07EjercicioB02.java
+++ Capitulo 07/C07EjercicioB02.java
@@ -2,23 +2,22 @@ public class C07EjercicioB02{
   public static void main (String [] args){
     final int ROWS = 4;
     final int COLUMNS = 5;
-    new int[][] arr = new int[ROWS][COLUMNS];
+    int[][] arr = new int[ROWS][COLUMNS];
     int input = 0;
     int acc = 0;
-    for(int i = 0; i <= ROWS; i++){
-      for(int j = 0; j <= COLUMNS; j++){
-        System.out.print("Introduzca el número que desea colocar en la posición");
+    for(int i = 0; i < ROWS; i++){
+      for(int j = 0; j < COLUMNS; j++){
+        System.out.print("Introduzca el número que desea colocar en la posición " + i + "," + j + ": ");
         input = Integer.parseInt(System.console().readLine());
       }
     }
     
     for(int i = 0; i <= ROWS; i++){
       for(int j = 0; j <= COLUMNS; j++){
-        if( i == ROWS ){
+        if( i == COLUMNS ){
           System.out.print("fila " + acc);
         }else{
-          input = Integer.parseInt(System.console().readLine());
-          
+          System.out.print(arr[i][j] + " ");
         }
       }
     }
