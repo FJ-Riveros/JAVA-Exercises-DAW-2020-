e5e79073be8e88c1bb025a7e5937d97bfd07b4b4
FJ-Riveros <fran.etnies@gmail.com>
2020-11-26 10:54:13 +0100

    Actualización

---
+++ Capitulo 07/C07EjercicioB07.java
@@ -0,0 +1,52 @@
+public class C07EjercicioB07 {
+  public static void main (String [] args){
+    final int ROWS = 4;
+    final int COLUMNS = 5;
+    String[][] arr = new String[ROWS][COLUMNS];
+    int minaX = 0;
+    int minaY = 0;
+    int tesoroX = 0;
+    int tesoroY = 0;
+    for(int i=0; i<ROWS; i++){
+      for(int j=0; j<COLUMNS; j++){
+        arr[i][j] = "█";
+      }
+    }
+    minaX = (int)(Math.random() * 5);
+    minaY = (int)(Math.random() * 4);
+    do{
+      tesoroX = (int)(Math.random() * 5);
+      tesoroY = (int)(Math.random() * 4);
+    }while( (tesoroX == minaX) && (tesoroY == minaY) );
+    arr[tesoroY][tesoroX] = "%";
+    arr[minaY][minaX] = "*";
+    System.out.println();
+    boolean fin = false;
+    do{
+      for(int i=0; i<ROWS; i++){
+        for(int j=0; j<COLUMNS; j++){
+          if( arr[i][j] == "*" || arr[i][j] == "%"){
+            System.out.print("█"+ " ");
+          }else {
+            System.out.print(arr[i][j] + " ");
+          }
+        }
+        System.out.println();
+        System.out.println();
+      }
+      System.out.print("Introduzca la coordenada de la fila(1-4): ");
+      int h = Integer.parseInt(System.console().readLine());
+      System.out.print("Introduzca la coordenada de la columna(1-5): ");
+      int w = Integer.parseInt(System.console().readLine());
+      if( arr[h - 1][w - 1] == "*" ){
+        System.out.print("Has tocado una mina, has perdido");
+        fin = true;
+      }else if(arr[h - 1][w - 1] == "%"){
+        System.out.print("Has ganado!");
+        fin = true;
+      }else{
+        arr[h - 1][w - 1] = "▒";
+      }
+    }while( fin == false );
+  }
+}
