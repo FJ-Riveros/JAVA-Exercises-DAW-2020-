0bd0bf20ab36d4ecab984196306ebf6fb9b76ee7
FJ-Riveros <fran.etnies@gmail.com>
2020-12-15 21:43:55 +0100

    actualizacion

--- EjerciciosPropios/EjercicioPropio05TableroAjedrez.java
+++ EjerciciosPropios/EjercicioPropio05TableroAjedrez.java
@@ -3,15 +3,24 @@ public class EjercicioPropio05TableroAjedrez{
     int[][] arr = new int[8][8];
     System.out.print("Introduzca la posición que desee: ");
     final String input = System.console().readLine();
-    int row = (int)input.charAt(0) - 49 ;
-    int column = (int)input.charAt(1) - 97;
-    System.out.println(row);
-    System.out.println(column);
-    /*for(int i=0; i<ROWS; i++){
-      for(int j=0; j<COLUMNS; j++){
-        System.out.print(arr[i][j]);
+    int y = input.charAt(0) - '1';
+    int x = input.charAt(1) - 'a';
+    int difY = 0;
+    int difX = 0;
+    String result = "";
+    char resultFilas,
+    resultColumnas;
+    System.out.print("Los posibles movimientos son: \n");
+    for(int i=0; i<8; i++){
+      for(int j=0; j<8; j++){
+        difY = Math.abs(i - y);
+        difX = Math.abs(j - x);
+        if(difY == difX && i != y && j != x){
+          resultFilas = (char)(i + '1');
+          resultColumnas = (char)(j + 'a');
+          System.out.print(resultFilas + "" + resultColumnas + " ");
+        }
       }
-      System.out.println();
-    }*/
+    }
   }
 }
