e98e3ef6abe6558ef8b38284d1bc35e60584faec
FJ-Riveros <fran.etnies@gmail.com>
2020-12-28 12:39:12 +0100

    actualizacion

--- Capitulo08/Matematicas/OperacionesEnteros.java
+++ Capitulo08/Matematicas/OperacionesEnteros.java
@@ -1,5 +1,11 @@
 package Matematicas;
 public class OperacionesEnteros{
+  
+  /*
+   * Nos dice si un int es primo o no.
+   * @param x número entero
+   * @return booleano true or false
+   */
   public static boolean esPrimo(int x){
     for(int i=2; i<x; i++){
       if(x%i==0){
@@ -9,6 +15,11 @@ public class OperacionesEnteros{
     return true; 
   }
 
+  /*
+   * Nos dice el siguiente primo de un int.
+   * @param x    número entero
+   * @return     número entero
+   */
   public static int siguientePrimo(int x){
     boolean check = false;
     do{
@@ -20,6 +31,12 @@ public class OperacionesEnteros{
     return x;
   }
   
+  /*
+   * Devuelve la potencia de una base y un exponente.
+   * @param base        base de la operación
+   * @param exponente   exponente de la operación
+   * @return            número entero, resultado de la potencia
+   */
   public static int potencia(int base, int exponente){
     int aux = base;
     if(exponente != 0){
@@ -32,6 +49,11 @@ public class OperacionesEnteros{
     }
   }
   
+  /*
+   * Devuelve la cantidad de dígitos de un número.
+   * @param x           número del que se desean saber los dígitos
+   * @return            número entero, cantidad de dígitos
+   */
   public static int digitos(long x){
     int counter = 0;
     while(x >0){
@@ -41,6 +63,11 @@ public class OperacionesEnteros{
     return counter;
   }
   
+  /*
+   * Devuelve un long dado la vuelta.
+   * @param x           long que se desea voltear
+   * @return            long, el número dado la vuelta
+   */
   public static long volteado(long x){
     long volteado = 0;
     while(x > 0){
@@ -50,10 +77,21 @@ public class OperacionesEnteros{
     return volteado;
   }
   
+  /*
+   * Devuelve el dígito deseado dentro de un long.
+   * @param x           long del que se desea copiar un dígito en concreto
+   * @param pos         posición del dígito dentro del long, empezando desde la izquierda y desde 0
+   * @return            número entero dígito seleccionado
+  */
   public static int digitoN(long x, int pos){
     return (int)((volteado(x) / potencia(10, pos))%10);
   }
   
+  /*
+   * Nos dice con un boolean si el long introducido es Capicua.
+   * @param x           long del que se desea saber si es Capicua
+   * @return            boolean true si es Capicua, false si no.
+  */
   public static boolean esCapicua(long x){
     if(volteado(x) == x){
       return true;
@@ -62,6 +100,12 @@ public class OperacionesEnteros{
     }
   }
   
+  /*
+   * Devuelve la posición de un dígito deseado dentro de un long, si no se encuentra devuelve -1.
+   * @param x               long del que se desea saber la posición de un digito en concreto
+   * @param digitoDeseado   dígito a buscar dentro del long
+   * @return                int, posición del dígito dentro del long si se encuentra, empezando desde la izquierda y desde 0, si no se encuentra devuelve -1
+  */
   public static int posicionDeDigito(long x, int digitoDeseado){
     x = volteado(x);
     int dig = digitos(x);
@@ -74,22 +118,53 @@ public class OperacionesEnteros{
     return -1;
   }
   
+  /*
+   * Quita la cantidad de dígitos deseada de derecha a izquierda de un long.
+   * @param x               long del que se desea eliminar digitos
+   * @param digitosQuitar   cantidad de dígitos a eliminar
+   * @return                long con los dígitos eliminados
+  */
   public static long quitaPorDetras(long x, int digitosQuitar){
     return x/(potencia(10, digitosQuitar));
   }
   
+  /*
+   * Quita la cantidad de dígitos deseada de izquierda a derecha de un long.
+   * @param x               long del que se desea eliminar digitos
+   * @param digitosQuitar   cantidad de dígitos a eliminar
+   * @return                long con los dígitos eliminados
+  */
   public static long quitaPorDelante(long x, int digitosQuitar){    
     return x%(potencia(10, (digitos(x)-digitosQuitar)));
   }
   
+  /*
+   * Añade un dígito al final de un long.
+   * @param x               long del que se desea añadir el digito
+   * @param digitoAdd       digito a añadir
+   * @return                long con el dígito añadido
+  */
   public static long pegaPorDetras(long x, int digitoAdd){
     return (x*10) + digitoAdd;
   }
   
+  /*
+   * Añade un dígito al principio de un long.
+   * @param x               long del que se desea añadir el digito
+   * @param digitoAdd       digito a añadir
+   * @return                long con el dígito añadido
+  */
   public static long pegaPorDelante(long x, int digitoAdd){
     return (digitoAdd * potencia(10, digitos(x))) + x;
   }
   
+  /*
+   * Seleccionamos solo un trozo de un long, según las posiciones introducidas por del usuario.
+   * @param x               long del que se desea seleccionar el trozo
+   * @param posEmpieza      posición inicial del trozo
+   * @param posTermina      posición final del trozo
+   * @return                long con el trozo deseado
+  */
   public static long trozoDeNumero(long x, int posEmpieza, int posTermina){
     int digitosTotales = digitos(x);
     long result = 0;
@@ -103,10 +178,22 @@ public class OperacionesEnteros{
     return result;
   }
   
+  /*
+   * Juntamos dos long introducidos por el usuario.
+   * @param x               primer long a juntar, este trozo será el comienzo del nuevo número
+   * @param y               segundo long a juntar, este trozo será el final del nuevo número
+   * @return                nuevo long con los dos números juntados
+  */
   public static long juntaNumeros(long x, long y){
     return (x * potencia(10,digitos(y))) + y;
   }
   
+  
+  /*
+   * Traducimos un número binario en decimal.
+   * @param x               número en binario a convertir
+   * @return                long en traducido a decimal
+  */
   public static long conversorBinarioDecimal(long x){
     int digits = Matematicas.OperacionesEnteros.digitos(x);
     int multiplicador = 1;
@@ -121,6 +208,11 @@ public class OperacionesEnteros{
     return result;
   }
   
+  /*
+   * Traducimos un número decimal en binario(forma lenta).
+   * @param x               número en decimal a convertir a binario
+   * @return                long en traducido a binario
+  */
   public static long conversorDecimalBinarioLento(long x){
     int counter = 0;
     int bits = 0;
@@ -144,6 +236,11 @@ public class OperacionesEnteros{
     return result;
   }
   
+  /*
+   * Traducimos un número decimal en binario(forma rápida).
+   * @param x               número en decimal a convertir a binario
+   * @return                long en traducido a binario
+  */
   public static long conversorDecimalBinarioRapido(long x){
     if(x == 0){
       return 0;
