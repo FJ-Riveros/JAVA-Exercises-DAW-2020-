ea27ea3866566d202074abb05831347f1573abd0
FJ-Riveros <fran.etnies@gmail.com>
2020-12-30 21:33:16 +0100

    actualizacion

--- Capitulo08/Matematicas/ManipulacionArraysUnidimensionales.java
+++ Capitulo08/Matematicas/ManipulacionArraysUnidimensionales.java
@@ -1,6 +1,10 @@
 package Matematicas;
 public class ManipulacionArraysUnidimensionales {
-  public static object generaArrayInt (int size, int minInterval, int maxInterval){
-    
+  public static int[] generaArrayInt (int size, int minInterval, int maxInterval){
+    int [] arr = new int[size];
+    for(int i=0; i<size; i++){
+      arr[i] = (int)(Math.random() * (maxInterval + 1 - minInterval) + minInterval);
+    }
+    return arr;
   }
 }
--- Capitulo08/pruebaEnteros.java
+++ Capitulo08/pruebaEnteros.java
@@ -6,6 +6,10 @@ public static void main(String [] args){
     //System.out.print("Introduzca el digito a buscar :" );
     int num = Integer.parseInt(System.console().readLine());
     int exponente = Integer.parseInt(System.console().readLine());
-    System.out.println("Los digitos que tiene son " + Matematicas.OperacionesEnteros.potencia(num, exponente));
+    //System.out.println("Los digitos que tiene son " + Matematicas.OperacionesEnteros.potencia(num, exponente));
+    int[] arr1 = Matematicas.ManipulacionArraysUnidimensionales.generaArrayInt(5, 10, 20);
+    for(int i=0; i< arr1.length; i++){
+      System.out.print(arr1[i] + " ");
+    }
   }
 }
