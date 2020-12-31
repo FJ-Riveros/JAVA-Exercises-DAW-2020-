f3e880a0372181394af350ce925f591243142a36
FJ-Riveros <fran.etnies@gmail.com>
2020-12-31 13:37:42 +0100

    actualizacion

--- Capitulo08/Matematicas/ManipulacionArraysBidimensionales.java
+++ Capitulo08/Matematicas/ManipulacionArraysBidimensionales.java
@@ -1,5 +1,15 @@
 package Matematicas;
 public class ManipulacionArraysBidimensionales{
+  /*  
+   * Crea un Array bidimensional de las filas y columnas deseadas, además de 
+   * rellenarla con números aleatorios dentro del mínimo y máximo valor introducido
+   * por el usuario.
+   * @param rows            Columnas que tendrá el Array bidi
+   * @param columns         Filas que tendrá el Array bidi
+   * @param minInterval     Valor máximo que contendrá el Array bidi
+   * @param maxInterval     Valor mínimo que contendrá el Array bidi
+   * @return                int[][] con el size y valores establecidos por el usuario
+  */
   public static int[][] generaArrayBiInt(int rows, int columns, int minInterval, int maxInterval){
     int[][] arr = new int[rows][columns];
     for(int i=0; i<rows; i++){
@@ -8,5 +18,19 @@ public class ManipulacionArraysBidimensionales{
       }
     }
     return arr;
+  }
+  
+  /*  
+   * Devuelve la fila que se indica del Array bidi como parámetro
+   * @param int[][]         Array bidimensional que introduce el usuario
+   * @param rowDeseada      Fila que se desea obtener del Array bidi
+   * @return                int[] con la fila deseada por el usuario 
+  */
+  public static int[] filaDeArrayBiInt(int[][] arr, int rowDeseada){
+    int[] arrResult = new int[arr[0].length];
+    for(int i=0; i<arr[0].length; i++){
+      arrResult[i] = arr[rowDeseada][i];
+    }
+    return arrResult;
   }
 }
--- Capitulo08/pruebaEnteros.java
+++ Capitulo08/pruebaEnteros.java
@@ -7,19 +7,26 @@ public static void main(String [] args){
     //int num = Integer.parseInt(System.console().readLine());
     //int exponente = Integer.parseInt(System.console().readLine());
     //System.out.println("Los digitos que tiene son " + Matematicas.OperacionesEnteros.potencia(num, exponente));
-    /*int[] prueba = new int[5];
-    for(int i=0; i< prueba.length; i++){
-      prueba[i] = (int)(Math.random() * (20 + 1 - 10) + 10);
+    //int[][] prueba = new int[5][5];
+    /*for(int i=0; i< prueba.length; i++){
+      prueba[i][j] = (int)(Math.random() * (20 + 1 - 10) + 10);
       System.out.print(prueba[i] + " ");
     }*/
     int[][] minimo= Matematicas.ManipulacionArraysBidimensionales.generaArrayBiInt(5,5, 10, 20);
+    int[] result = Matematicas.ManipulacionArraysBidimensionales.filaDeArrayBiInt(minimo, 4);
     //System.out.println("Finish: " + minimo);
     System.out.println();
-    for(int i=0; i<5; i++){
-      for(int j=0; j<5; j++){
+    for(int i=0; i<minimo.length; i++){
+      for(int j=0; j<minimo[0].length; j++){
         System.out.print(minimo[i][j] + " ");
       }
       System.out.println();
+    }
+    
+    System.out.println();
+    System.out.println();
+    for(int i=0; i<result.length; i++){
+      System.out.print(result[i] + " ");
     }
   }
   
