e31aa03db04402dadda7c7166b0e18507d4eb8b9
FJ-Riveros <fran.etnies@gmail.com>
2021-01-05 13:48:21 +0100

    actualizacion

--- Capitulo08/Matematicas/ManipulacionArraysUnidimensionales.java
+++ Capitulo08/Matematicas/ManipulacionArraysUnidimensionales.java
@@ -165,4 +165,27 @@ public class ManipulacionArraysUnidimensionales {
     return arr;
   }
   
+  /*
+   * Devuelve un Array nuevo con todos los primos del array original
+   * @param arr       Array original del que obtener los primos
+   * @return          Array con los primos del array original, si no hay ninguno
+   *                  se devuelve un Array con el item -1.
+  */
+  public static int[] filtraPrimos(int[] arr){
+    int counter = 0;
+    int[] aux = new int[arr.length];
+    for(int item: arr){
+      if(esPrimo(item)) aux[counter++] = item;
+    }
+    if(counter != 0){
+      int[] result = new int[counter];
+      for(int i=0; i<counter; i++){
+        result[i] = aux[i];
+      }
+      return result;
+    }else{
+      int[] result = {-1};
+      return result;
+    }
+  }
 }
