d6b32fa1b72878471d79b056fdeec88e94cfda88
FJ-Riveros <fran.etnies@gmail.com>
2021-01-07 22:36:24 +0100

    actualizacion

--- Capitulo08/Matematicas/ManipulacionArraysUnidimensionales.java
+++ Capitulo08/Matematicas/ManipulacionArraysUnidimensionales.java
@@ -1,4 +1,5 @@
 package Matematicas;
+import static Matematicas.OperacionesEnteros.esCapicua;
 //Ejercicios 20-28
 public class ManipulacionArraysUnidimensionales {
   /*
@@ -176,6 +177,31 @@ public class ManipulacionArraysUnidimensionales {
     int[] aux = new int[arr.length];
     for(int item: arr){
       if(esPrimo(item)) aux[counter++] = item;
+    }
+    if(counter != 0){
+      int[] result = new int[counter];
+      for(int i=0; i<counter; i++){
+        result[i] = aux[i];
+      }
+      return result;
+    }else{
+      int[] result = {-1};
+      return result;
+    }
+  }
+  
+  /*
+   * Devuelve un Array nuevo con todos los números capicua que se encuentren en 
+   * el array original .
+   * @param arr       Array original del que obtener los números capicua
+   * @return          Array con los capicua del array original, si no hay ninguno
+   *                  se devuelve un Array con el item -1.
+  */
+  public static int[] filtraCapicua(int[] arr){
+    int counter = 0;
+    int[] aux = new int[arr.length];
+    for(int item: arr){
+      if(esCapicua(item)) aux[counter++] = item;
     }
     if(counter != 0){
       int[] result = new int[counter];
