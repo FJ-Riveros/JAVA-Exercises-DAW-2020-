aca40a480cb576321a98995930c7e8d6343835e8
FJ-Riveros <fran.etnies@gmail.com>
2021-01-08 15:12:34 +0100

    actualizacion

--- Capitulo08/Matematicas/creacionDeFiguras.java
+++ Capitulo08/Matematicas/creacionDeFiguras.java
@@ -16,4 +16,25 @@ public class creacionDeFiguras{
     }
     return result;
   }
+  
+  /*
+   * Esta función nos devuelve un String con el caracter deseado sólo en la 
+   * primera posición y en la última, las centrales son espacios.
+   * 
+   * @param character         Carácter con el que queremos que se repita
+   * @param repeticiones      Int con el tamaño horizontal que queramos
+   * @return                  String con los carácteres repetidos y los centra
+   *                          les vacios.
+  */ 
+  public static String printLineaEspaciosCentrales(char caracter, int repeticiones){
+    String result = "";
+    for(int i=0; i<repeticiones; i++){
+      if(i==0 || i==repeticiones-1){
+        result += caracter;
+      }else{
+        result += " ";
+      }
+    }
+    return result;
+  }
 }
