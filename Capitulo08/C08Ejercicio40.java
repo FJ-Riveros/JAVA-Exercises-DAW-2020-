9e460fd2d0fbb9e6b34e6688fd680e9ff383585a
FJ-Riveros <fran.etnies@gmail.com>
2021-01-08 10:01:31 +0100

    actualizacion

--- Capitulo08/C08Ejercicio40.java
+++ Capitulo08/C08Ejercicio40.java
@@ -3,10 +3,10 @@ import static Matematicas.OperacionesEnteros.digitoN;
 
 public class C08Ejercicio40{
   public static void main(String [] args){
-    int[] prueba = {3, 32, 47, 907};
+    int[] prueba = {0, 17, 07, 906};
     int[] result = filtraCon7(prueba);
-    for(int item: result){
-      System.out.print(item + " ");
+    for(int i=0; i<result.length; i++){
+      System.out.print(result[i] + " ");
     }
   }
   
@@ -16,26 +16,34 @@ public class C08Ejercicio40{
    * @param x           Array input a filtrar
    * @return            Array con los items que contengan un 7
   */
-  
-  //Terminar
   public static int[] filtraCon7(int x[]){
     int[] aux = new int[x.length];
     int currentNummer = 0;
-    int currentDigits = 0;
+    int totalDigits = 0;
     boolean check = false;
     int counter = 0;
     int counterArr = 0;
+    int currentDigit = 0;
     for(int i=0; i<x.length; i++){
       currentNummer = x[i];
-      currentDigits = digitos(currentNummer);
+      totalDigits = digitos(currentNummer);
       check = false;
       counter = 0;
       do{
-        if(digitoN(currentNummer,i) == 7) check = true;
+        currentDigit = digitoN(currentNummer,counter);
+        if( currentDigit == 7) check = true;
         counter++;
-      }while(counter<currentDigits && digitoN(currentNummer,i)!= 7);
+      }while(counter<totalDigits && currentDigit != 7);
       if(check) aux[counterArr++] = currentNummer;
     }
-    return aux;
+    if(counterArr != 0){
+      int[] result = new int[counterArr];
+      for(int i=0; i<counterArr; i++){
+        result[i] = aux[i];
+      }
+      return result;
+    }
+    int[] result = {-1};
+    return result;
   }
 }
