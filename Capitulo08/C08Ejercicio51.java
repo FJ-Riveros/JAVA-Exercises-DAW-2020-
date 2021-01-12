8ac949716102d91eb93ac4e065e2f34c4dfaf4e1
FJ-Riveros <fran.etnies@gmail.com>
2021-01-12 11:15:28 +0100

    actualizacion

--- Capitulo08/C08Ejercicio51.java
+++ Capitulo08/C08Ejercicio51.java
@@ -1,14 +1,21 @@
-import Matematicas.OperacionesEnteros.esCapicua;
+import static Matematicas.OperacionesEnteros.esCapicua;
+import static Matematicas.OperacionesEnteros.esPrimo;
 
 public class C08Ejercicio51{
   public static void main(String [] args){
+    String[] primo = {"no es primo", "es primo"};
+    String[] capicua = {"no es capicúa", "es capicúa"};
     int[] arr = new int[10];
+    String primo = "";
+    String capicua = "";
     for(int item: arr){
       item = (int)(Math.random()*(100+1-2))+2;
       System.out.print(item + " ");
     }
     for(int item: arr){
-      System.out.println("El " + item + );
+      capicua = esCapicua((long)item) ? "es capicúa" : "no es capicúa";
+      primo = esPrimo((long)item) ? "es primo" : "no es primo";
+      System.out.println("El " + item + primo + " y " + capicua);
     }
   }
 }
