36c93004294d24c54e56621e60d73a03f4164da9
FJ-Riveros <fran.etnies@gmail.com>
2021-01-18 10:23:33 +0100

    actualizacion

--- EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa19_01.java
+++ EjerciciosExamenes/Ejercicios sorpresa/EjercicioSorpresa19_01.java
@@ -3,9 +3,9 @@ public class EjercicioSorpresa19_01{
     System.out.print("Introduzca el número de items del Array: ");
     final int SIZE = Integer.parseInt(System.console().readLine());
     int[] input = new int[SIZE];
-    System.out.print("Introduzca el valor máximo posible: ");
+    System.out.print("Introduzca el primer valor del rango: ");
     final int MAX = Integer.parseInt(System.console().readLine());
-    System.out.print("Introduzca el valor mínimo posible: ");
+    System.out.print("Introduzca el segundo valor del rango: ");
     final int MIN = Integer.parseInt(System.console().readLine());
     rellenaArrayUni(input, MAX, MIN);
     System.out.println("Array original: ");
@@ -55,6 +55,13 @@ public class EjercicioSorpresa19_01{
     System.out.print("───┘");
   }
   public static void rellenaArrayUni(int[] input, int max, int min){
+    int temp = 0;
+    if(min > max){
+      temp = max;
+      max = min;
+      min = temp;
+    }
+    
     for(int i=0; i<input.length; i++){
       input[i] = (int)(Math.random()* (max+1-min)+min);
     }
