8f9cc50eeb6c3581b01b7433b93fe1e8bdc524e5
FJ-Riveros <fran.etnies@gmail.com>
2021-01-24 14:26:50 +0100

    actualizacion

--- EjerciciosExamenesAñoPasado/Control03/Ejercicio04.java
+++ EjerciciosExamenesAñoPasado/Control03/Ejercicio04.java
@@ -4,25 +4,59 @@ public class Ejercicio04{
     String[] aux = input.clone();
     int random = (int)(Math.random()* input.length);
     String result = desordenaPalabra(aux[1]);
+    //System.out.print(result);
   }
   
   public static String desordenaPalabra(String word){
-    String[] arrayFromWord = convierteStringEnArray(word);
-    String[] muster = arrayFromWord.clone();
-    boolean check = false;
+    char[] arrayFromWord = convierteStringEnArray(word);
+    char[] muster = arrayFromWord.clone();
+    boolean check = true;
+    int randomA = 0;
+    int randomB = 0;
+    char temp = '0';
     do{
-      check = false;
-      //Seguir
-      
-    }while();
+      check = compruebaValoresArray(muster);
+      if(!check){
+        randomA = (int)(Math.random()*muster.length);
+        if(muster[randomA] != '*'){
+          do{
+            randomB = (int)(Math.random()*muster.length);
+          }while(randomA == randomB || muster[randomB] == '*');
+          temp = muster[randomA];
+          arrayFromWord[randomA] = arrayFromWord[randomB];
+          arrayFromWord[randomB] = temp;
+          muster[randomA] = '*';
+          muster[randomB] = '*';
+        } 
+      }
+    }while(!check);
+    for(char item: muster) System.out.print(item + " ");
+    word = convierteArrayEnString(arrayFromWord);
     return word;
   }
   
-  public static String[] convierteStringEnArray(String input){
-    String[] result = new String[input.length()];
+  public static char[] convierteStringEnArray(String input){
+    char[] result = new char[input.length()];
     for(int i=0; i<input.length(); i++){
       result[i] = input.charAt(i);
     }
+    /*for(int i=0; i<result.length; i++){
+      System.out.print(result[i] + " ");
+    } */
+    return result;
+  }
+  
+  public static boolean compruebaValoresArray(char[] input){
+    boolean check = true;
+    for(int i=0; i<input.length; i++){
+        if(input[i] != '*') check = false;
+    }
+    return check;
+  }
+  
+  public static String convierteArrayEnString(char[] input){
+    String result = "";
+    for(int i=0; i<input.length; i++) result+= input[i];
     return result;
   }
 }
