d90ead8ee378dd31c00e8704e8df8bd0f65d7f19
FJ-Riveros <fran.etnies@gmail.com>
2021-01-25 20:37:41 +0100

    actualizacion

--- EjerciciosExamenesAñoPasado/Ejercicios sorpresa/EjercicioSorpresaModular.java
+++ EjerciciosExamenesAñoPasado/Ejercicios sorpresa/EjercicioSorpresaModular.java
@@ -1,16 +1,19 @@
 public class EjercicioSorpresaModular{
   public static void main(String [] args){
     System.out.print("Indique las filas del Array:" );
-    final int  FILAS= Integer.parseInt(System.console().readLine());
+    final int  FILAS = Integer.parseInt(System.console().readLine());
+    System.out.print("Indique las columnas del Array:" );
+    final int  COLUMNAS = Integer.parseInt(System.console().readLine());
     System.out.print("Indique el primer valor para rellenar el Array:" );
     final int VALORA = Integer.parseInt(System.console().readLine());
     System.out.print("Indique el segundo valor para rellenar el Array:");
     final int VALORB = Integer.parseInt(System.console().readLine());
-    int[][] arr = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-    presentaArrayBidimensional(arr, "filas");
-    presentaArrayBidimensional(arr, "filainvertida");
-    presentaArrayBidimensional(arr, "columna");
-    presentaArrayBidimensional(arr, "columnainvertida");
+    //int[][] arr = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    int[][] prueba = rellenaYCreaArrayConDosValores(FILAS,COLUMNAS,VALORA,VALORB);
+    presentaArrayBidimensional(prueba, "filas");
+    presentaArrayBidimensional(prueba, "filainvertida");
+    presentaArrayBidimensional(prueba, "columna");
+    presentaArrayBidimensional(prueba, "columnainvertida");
     
   }
     public static void presentaArrayBidimensional(int[][] input, String formato){
@@ -19,16 +22,22 @@ public class EjercicioSorpresaModular{
       int igualadorColumnas = 0;
       int igualadorFilas = input.length-1;
       switch(formato){
-        case
-      }
-      if(formato.equals("filainvertida")) igualadorColumnas = input[0].length-1;
-      if(formato.equals("columna")) igualadorFilas = 0;
-      if(formato.equals("columna") || 
-      formato.equals("columnainvertida")){
+        case "filainvertida":
+          igualadorColumnas = input[0].length-1;
+        break;
+        
+        case "columna":
+        igualadorFilas = 0;
         finVariableA = input[0].length;
         finVariableB = input.length;
+        break;
+        
+        case "columnainvertida":
+        finVariableA = input[0].length;
+        finVariableB = input.length;
+        break;
+        default:
       }
-      
       for(int i=0; i<finVariableA; i++){
         for(int j=0; j<finVariableB; j++){
           if(formato.equals("filas") || formato.equals("filainvertida")){
@@ -41,11 +50,24 @@ public class EjercicioSorpresaModular{
       }
       System.out.println();
     }
-}  
-    /*public static int[][] rellenaYCreaArrayConDosValores(int FILAS, int COLUMNAS,
+ 
+    public static int[][] rellenaYCreaArrayConDosValores(int FILAS, int COLUMNAS,
     int VALORA, int VALORB){
-      
-      
-    }*/
-  
-
+      int[][] result;
+        
+      if(VALORA>VALORB) {
+        int aux;
+        aux = VALORB;
+        VALORB = VALORA;
+        VALORA = aux;
+      }
+    
+      result = new int [FILAS][COLUMNAS];
+      for (int i=0; i<FILAS; i++) {
+        for (int j=0; j<COLUMNAS; j++) {
+          result[i][j] = (int) (Math.random()*(VALORB-VALORA+1))+VALORA;
+        }
+      }
+      return result;
+    }  
+}  
