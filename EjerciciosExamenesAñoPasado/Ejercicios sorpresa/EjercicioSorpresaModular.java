9f27f614d40d594d1f3930d4a5658bc436e234f6
FJ-Riveros <fran.etnies@gmail.com>
2021-01-25 20:10:20 +0100

    actualizacion

--- EjerciciosExamenesAñoPasado/Ejercicios sorpresa/EjercicioSorpresaModular.java
+++ EjerciciosExamenesAñoPasado/Ejercicios sorpresa/EjercicioSorpresaModular.java
@@ -6,50 +6,40 @@ public class EjercicioSorpresaModular{
     final int VALORA = Integer.parseInt(System.console().readLine());
     System.out.print("Indique el segundo valor para rellenar el Array:");
     final int VALORB = Integer.parseInt(System.console().readLine());
-    int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+    int[][] arr = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    presentaArrayBidimensional(arr, "filas");
+    presentaArrayBidimensional(arr, "filainvertida");
     presentaArrayBidimensional(arr, "columna");
+    presentaArrayBidimensional(arr, "columnainvertida");
+    
   }
     public static void presentaArrayBidimensional(int[][] input, String formato){
-      int comienzoFilas = 0;
-      int finFilas = 0;
-      int comienzoColumnas = 0;
-      int finColumnas = 0;
-      int igualadorFilas = 0;
+      int finVariableA = input.length;
+      int finVariableB= input[0].length;
       int igualadorColumnas = 0;
+      int igualadorFilas = input.length-1;
       switch(formato){
-        case "filas":
-        igualadorColumnas = 0;
-        break;
-        
-        case "filainvertida":
-          igualadorColumnas = input[0].length-1;
-        break;
-        
-        case "columna":
-          igualadorColumnas = 0;
-          igualadorFilas = 0;
-          
-        break;
-        
-        case "columnainvertida":
-          comienzoFilas = 0;
-          finFilas = input.length;
-          comienzoColumnas = 0;
-          finColumnas = input[0].length;
-          igualadorColumnas = input[0].length;
-          igualadorFilas = input.length;
-        break;
+        case
       }
-      for(int i=0; i<input.length; i++){
-        for(int j=0; j<input[0].length; j++){
+      if(formato.equals("filainvertida")) igualadorColumnas = input[0].length-1;
+      if(formato.equals("columna")) igualadorFilas = 0;
+      if(formato.equals("columna") || 
+      formato.equals("columnainvertida")){
+        finVariableA = input[0].length;
+        finVariableB = input.length;
+      }
+      
+      for(int i=0; i<finVariableA; i++){
+        for(int j=0; j<finVariableB; j++){
           if(formato.equals("filas") || formato.equals("filainvertida")){
             System.out.printf("%4d", input[i][Math.abs(igualadorColumnas-j)]);
           }else{
-            System.out.printf("%4d", input[Math.abs(igualadorColumnas-j)][igualadorFilas]);
+            System.out.printf("%4d", input[Math.abs(igualadorFilas-j)][Math.abs(igualadorColumnas-i)]);
           }
         }
         System.out.println();
       }
+      System.out.println();
     }
 }  
     /*public static int[][] rellenaYCreaArrayConDosValores(int FILAS, int COLUMNAS,
