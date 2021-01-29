1aec78cb2851f45581b2fd2ae3155c8cd732863b
FJ-Riveros <fran.etnies@gmail.com>
2021-01-29 19:43:39 +0100

    actualizacion

--- EjerciciosExamenesAñoPasado/Control03/libreriaPalotes/funcionesPalotes.java
+++ EjerciciosExamenesAñoPasado/Control03/libreriaPalotes/funcionesPalotes.java
@@ -6,4 +6,25 @@ public class funcionesPalotes{
     int suma = conversorA+conversorB;
     return funcionesAuxiliaresPalotes.convierteDecimalEnPalotes(suma);
   }
+  
+  public static String restaPalotes(String input1, String input2){
+    int conversorA = funcionesAuxiliaresPalotes.conviertePalotesEnDecimal(input1);
+    int conversorB = funcionesAuxiliaresPalotes.conviertePalotesEnDecimal(input2);
+    int resta = conversorA>conversorB ? conversorA-conversorB : conversorB-conversorA;
+    return funcionesAuxiliaresPalotes.convierteDecimalEnPalotes(resta);
+  }
+  
+  public static String divisionPalotes(String input1, String input2){
+    int conversorA = funcionesAuxiliaresPalotes.conviertePalotesEnDecimal(input1);
+    int conversorB = funcionesAuxiliaresPalotes.conviertePalotesEnDecimal(input2);
+    int division = conversorA>conversorB ? conversorA/conversorB : conversorB/conversorA;
+    return funcionesAuxiliaresPalotes.convierteDecimalEnPalotes(division);
+  }
+  
+  public static String restoPalotes(String input1, String input2){
+    int conversorA = funcionesAuxiliaresPalotes.conviertePalotesEnDecimal(input1);
+    int conversorB = funcionesAuxiliaresPalotes.conviertePalotesEnDecimal(input2);
+    int resto = conversorA%conversorB;
+    return funcionesAuxiliaresPalotes.convierteDecimalEnPalotes(resto);
+  }
 }
--- EjerciciosExamenesAñoPasado/Control03/pruebaPalotes.java
+++ EjerciciosExamenesAñoPasado/Control03/pruebaPalotes.java
@@ -3,6 +3,9 @@ public class pruebaPalotes{
   public static void main (String[] args){
     String input1 = "|||";
     String input2 = "||-|";
-    System.out.print(libreriaPalotes.funcionesPalotes.sumaPalotes(input1,input2));
+    System.out.println(libreriaPalotes.funcionesPalotes.sumaPalotes(input1,input2));
+    System.out.println(libreriaPalotes.funcionesPalotes.restaPalotes(input1,input2));
+    System.out.println(libreriaPalotes.funcionesPalotes.divisionPalotes(input1,input2));
+    System.out.println(libreriaPalotes.funcionesPalotes.restoPalotes(input1,input2));
   } 
 }
