0ec7fc646688ae6223ae410b67a2346988b72462
FJ-Riveros <fran.etnies@gmail.com>
2021-02-04 21:33:51 +0100

    actualizacion

--- Capitulo08/diagonalMatrizBidi.java
+++ Capitulo08/diagonalMatrizBidi.java
@@ -1,35 +1,51 @@
 import RecogidaDeDatos.Entrada;
 public class diagonalMatrizBidi{
   public static void main(String[] args){
-    //TERMINAR TENIENDO EN CUENTA EL CASO DE QUE ES MUY LIOSO, CAMBIAR EL INPUT DEL USER CON NUMEROS
     System.out.print("Introduzca las filas del Array: ");
     final int SIZEROWS = RecogidaDeDatos.Entrada.Entero();
     System.out.print("Introduzca las columnas del Array: ");
     final int SIZECOLUMNS = RecogidaDeDatos.Entrada.Entero();
     String[][] input = new String[SIZEROWS][SIZECOLUMNS];
-    System.out.print("Introduzca la dirección de la diagonal: ");
+    int[] helper = {input.length-1, input[0].length-1};
+    System.out.println("Seleccione la diagonal: ");
+    System.out.println("1- Desde la esquina superior izquierda.");
+    System.out.println("2- Desde la esquina superior derecha.");
+    System.out.println("3- Desde la esquina infieror izquierda.");
+    System.out.println("4- Desde la esquina inferior derecha.");
     final int DIRECTION = RecogidaDeDatos.Entrada.Entero();
-    System.out.print("Introduzca la fila desde donde quiere comenzar: ");
-    final int ROW = RecogidaDeDatos.Entrada.Entero();
-    System.out.print("Introduzca la columna desde donde quiere comenzar: ");
-    final int COLUMN = RecogidaDeDatos.Entrada.Entero();
+    int x = 0;
+    int y = 0;
+    switch( DIRECTION){
+      case 2:
+        x = helper[1];
+      break;
+      
+      case 3:
+        y = helper[0];
+      break;
+      
+      case 4:
+        x = helper[1];
+        y = helper[0];
+      break;
+      
+      default:
+    }
     rellenaArrayConBlanco(input);
-    devuelveDiagonal(input, ROW,COLUMN);
-    
-    
+    devuelveDiagonal(input, y,x);
   }
-  
-  public static void devuelveDiagonal(String[][] input, int ROW, int COLUMN){
-    int selectorX = COLUMN;
-    int selectorY = ROW;
+    
+  public static void devuelveDiagonal(String[][] input, int comienzoY, int comienzoX){
+    int selectorX = comienzoX;
+    int selectorY = comienzoY;
     for(int i=0; i<input.length; i++){
-      if(ROW == 0 && COLUMN == 0){
+      if(comienzoY == 0 && comienzoX == 0){
         input[selectorY++][selectorX++] = "██";
-      }else if(ROW==0 && COLUMN==input[0].length-1){
+      }else if(comienzoY==0 && comienzoX==input[0].length-1){
         input[selectorY++][selectorX--] = "██";
-      }else if(ROW==input.length-1 && COLUMN==0){
+      }else if(comienzoY==input.length-1 && comienzoX==0){
         input[selectorY--][selectorX++] = "██";
-      }else if(ROW==input.length-1 && COLUMN==input[0].length-1){
+      }else if(comienzoY==input.length-1 && comienzoX==input[0].length-1){
         input[selectorY--][selectorX--] = "██";
       }
     }
