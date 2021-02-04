24ef9b84ad0c763414aaddb0f4a713a05d1b1aed
FJ-Riveros <fran.etnies@gmail.com>
2021-02-04 10:08:15 +0100

    actualizacion

---
+++ Capitulo08/diagonalMatrizBidi.java
@@ -0,0 +1,24 @@
+import RecogidaDeDatos.Entrada*;
+public class diagonalMatrizBidi{
+  public static void main(String[] args){
+    String[] input = new String[4][6];
+    System.out.print("Introduzca la fila desde donde quiere comenzar: ");
+    final int ROW = Entrada.Entero();
+    System.out.print("Introduzca la columna desde donde quiere comenzar: ");
+    final int COLUMN = Entrada.Entero();
+    
+  }
+  
+  public static void devuelveDiagonal(String[] input, int ROW, int COLUMN){
+    int selectorX = COLUMN;
+    int selectorY = ROW;
+    for(int i=0; i<input.length; i++){
+      if(ROW == 0 && COLUMN == 0){
+        
+      }
+      System.out.print("
+    }
+  }
+  
+  
+}
