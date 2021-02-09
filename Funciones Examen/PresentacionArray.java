80635ac0caff12e6195e1108e6b5cd54a39760ee
FJ-Riveros <fran.etnies@gmail.com>
2021-02-09 22:10:27 +0100

    actualizacion

--- Funciones Examen/PresentacionArray.java
+++ Funciones Examen/PresentacionArray.java
@@ -1,6 +1,7 @@
 public class PresentacionArray{
   public static void main(String[] args){
-    System.out.print("Introduzca el número de items del Array: ");
+    int[][] pruebaBidi = {{1,100,1000},{4,65,31},{2000,23,10200}};
+    /*System.out.print("Introduzca el número de items del Array: ");
     final int SIZE = Integer.parseInt(System.console().readLine());
     int[] input = new int[SIZE];
     System.out.print("Introduzca un valor: ");
@@ -8,15 +9,14 @@ public class PresentacionArray{
     System.out.print("Introduzca otro valor: ");
     final int MIN = Integer.parseInt(System.console().readLine());
     rellenaArrayUni(input, MAX, MIN);
-    presentaArrayFormateado(input);
+    presentaArrayFormateado(input);*/
+    //System.out.print(digitosMaxArray(pruebaBidi));
+    presentaArrayFormateado(pruebaBidi);
 
   }
   
   public static void presentaArrayFormateado(int[] arr){
     int digitosMax = digitosMaxArray(arr);
-    System.out.print(digitosMax);
-    System.out.print("El Array tiene un tamaño de: " + arr.length);
-    System.out.println();
     System.out.print("┌");
     for(int i=0; i<arr.length; i++){
       for(int j=0; j<digitosMax; j++){
@@ -44,10 +44,16 @@ public class PresentacionArray{
         System.out.print("─");
       }
       if(i == arr.length-1){
-        System.out.print("┘");
+        System.out.print("┘\n");
       }else{
         System.out.print("┴");
       }
+    }
+  }
+  
+  public static void presentaArrayFormateado(int[][] arr){
+    for(int i=0; i<arr.length; i++){
+      presentaArrayFormateado(arr[i]);
     }
   }
   public static void rellenaArrayUni(int[] input, int max, int min){
@@ -67,6 +73,14 @@ public class PresentacionArray{
       if(cuentaDigitos(input[i]) > max){
         max = cuentaDigitos(input[i]);
       }
+    }
+    return max;
+  }
+  
+  public static int digitosMaxArray(int[][] input){
+    int max = 0;
+    for(int i=0; i<input.length; i++){
+      max = digitosMaxArray(input[i]);
     }
     return max;
   }
