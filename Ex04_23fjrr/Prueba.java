f90d1a8f17a1336e44b48b95699af4841deb3ced
FJ-Riveros <fran.etnies@gmail.com>
2021-02-11 12:34:58 +0100

    actualizacion

--- Ex04_23fjrr/Prueba.java
+++
@@ -1,8 +0,0 @@
-import FuncionesAuxCuenta.FuncionesCuenta;
-public class Prueba{
-  public static void main(String[] args){
-    String[] arrString = {"Hola"};
-    double[] Saldo = {245.00};
-    System.out.print(FuncionesAuxCuenta.FuncionesCuenta.informeTitular("Hola", arrString, Saldo));
-  }
-}
