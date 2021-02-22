298c68f65b6a19e052d530368fe750717b1c671a
FJ-Riveros <fran.etnies@gmail.com>
2021-02-22 20:30:03 +0100

    actualizacion

--- Capitulo 09/Caballo.java
+++ Capitulo 09/Caballo.java
@@ -1,6 +1,11 @@
 public class Caballo{
   float altura, peso;
   int edad;
+  
+  char[] foodEnergy = new char[10];
+  char[] sleepEnergy = new char[10];
+  char[] water = new char[10];
+  
   Caballo(){
     this.altura = (float)0;
     this.peso = (float)0;
@@ -24,13 +29,35 @@ public class Caballo{
     this.peso = inputPeso;
     this.edad = inputEdad;
   }
-  
-  public  String devuelveDatosCaballo(){
+  //Helper
+  public int devuelveNivel(char[] input){
+    int count = 0;
+    boolean check = true;
+    for(int i=0; i<input.length&&check; i++){
+      if(input[i]==#){
+        count++;
+      }else{
+        check = false;
+      } 
+    }
+    return count;
+  }
+  //Setters
+  public void alimentaCaballo(){
+    
+  }
+  //Getters
+  public String devuelveDatosCaballo(){
     String info = "";
     info = "Altura: " + this.altura + "\n";
     info = info + "Peso: " + this.peso + "\n";
     info = info + "Edad: " + this.edad + "\n";
     return info;
   }
+  
+  public String
+  
+  
+
   
 }
