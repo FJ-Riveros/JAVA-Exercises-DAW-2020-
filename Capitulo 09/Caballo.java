22df6a4d1fe2e37eef0f8c7d030b0dcec17aba78
FJ-Riveros <fran.etnies@gmail.com>
2021-02-22 20:11:48 +0100

    actualizacion

--- Capitulo 09/Caballo.java
+++ Capitulo 09/Caballo.java
@@ -1,6 +1,36 @@
 public class Caballo{
-  
+  float altura, peso;
+  int edad;
   Caballo(){
-  
+    this.altura = (float)0;
+    this.peso = (float)0;
+    this.edad = -1;
   }
+  
+  Caballo(float inputAltura){
+    this.altura = inputAltura;
+    this.peso = (float)0;
+    this.edad = -1;
+  }
+  
+  Caballo(float inputAltura, float inputPeso){
+    this.altura = inputAltura;
+    this.peso = inputPeso;
+    this.edad = -1;
+  }
+  
+  Caballo(float inputAltura, float inputPeso, int inputEdad){
+    this.altura = inputAltura;
+    this.peso = inputPeso;
+    this.edad = inputEdad;
+  }
+  
+  public  String devuelveDatosCaballo(){
+    String info = "";
+    info = "Altura: " + this.altura + "\n";
+    info = info + "Peso: " + this.peso + "\n";
+    info = info + "Edad: " + this.edad + "\n";
+    return info;
+  }
+  
 }
--- Capitulo 09/Ejercicio02.java
+++ Capitulo 09/Ejercicio02.java
@@ -1,5 +1,6 @@
 public class Ejercicio02 {
   public static void main(String[] args){
-  
+    Caballo Pegaso = new Caballo((float)1.52, (float)30.2, 5);
+    System.out.print(Pegaso.devuelveDatosCaballo());
   }
 }
