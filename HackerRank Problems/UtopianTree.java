65243776ea9e6447c32b3687fc2ca53426810769
FJ-Riveros <fran.etnies@gmail.com>
2021-02-26 12:24:38 +0100

    actualizacion

---
+++ HackerRank Problems/UtopianTree.java
@@ -0,0 +1,16 @@
+public class UtopianTree{
+  public static void main(String[] args){
+    int result = 0;
+    for(int i = 0; i < Integer.parseInt(args[0]); i++){
+      result = 0;
+      for(int j= 0; j<=Integer.parseInt(args[i+1]); j++){
+        if(j%2==0){
+          result += 1;
+        }else{
+          result *= 2;
+        }
+      }
+      System.out.println(result);
+    }
+  }
+}
