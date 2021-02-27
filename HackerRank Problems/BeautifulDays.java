cc1e575f7d28469e2a1dfbf33df3b44ea7bbd23d
FJ-Riveros <fran.etnies@gmail.com>
2021-02-27 17:58:08 +0100

    actualizacion

--- HackerRank Problems/BeautifulDays.java
+++ HackerRank Problems/BeautifulDays.java
@@ -1,6 +1,17 @@
 //https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
 public class BeautifulDays{
   public static void main(String[] args){
-    
+    int counter = 0;
+    int reverse = 0;
+    int aux = 0;
+    for(int i=Integer.parseInt(args[0]); i<=Integer.parseInt(args[1]); i++){
+      aux = i;
+      while(aux>0){
+        reverse = (reverse * 10) + (reverse + aux%10);
+	aux/= 10;
+      }
+      if((i-reverse)%Integer.parseInt(args[2]) == 0) counter++;
+    }
+    System.out.println(counter);
   }
 }
