f84c754e12541491878d88a4880855438a92c728
FJ-Riveros <fran.etnies@gmail.com>
2021-03-15 09:39:27 +0100

    actualizacion

--- Capitulo 09/Ejercicio 9/Movil.java
+++ Capitulo 09/Ejercicio 9/Movil.java
@@ -1,26 +1,28 @@
 public class Movil extends Terminal{
-  private float importe = 0;
-  private String tarifa;
+  private int costePorMinuto;
+  private double totalTarificado;
   
   public Movil(String num, String tar){
     super(num);
-    this.tarifa = tar;
+    if(tar.equals("rata")){
+      this.costePorMinuto = 6;
+    }else if(tar.equals("mono")){
+      this.costePorMinuto = 12;
+    }else{
+      this.costePorMinuto = 3;
+    }
+    this.totalTarificado = 0;
   }
   
   public void llama(Terminal destinatario, int duracion){
     super.llama(destinatario, duracion);
-    if(this.tarifa.equals("rata")){
-      this.importe += duracion/60 * 0.06;
-    }else if(this.tarifa.equals("mono")){
-      this.importe += duracion/60 * 0.12;
-    }else{
-      this.importe += duracion/60 * 0.3;
-    }
+    float costeEnEstaLlamada = duracion / (float)60;
+    this.totalTarificado += (costePorMinuto / (float)100) * costeEnEstaLlamada;
   }
   
   public String toString(){
     String resultado = super.toString();
-    resultado = resultado + " - tarificados "+ this.importe + " euros.";
+    resultado = resultado + " - tarificados "+ String.format("%.2f", this.totalTarificado) + " euros.";
     return resultado;
   }
 }
