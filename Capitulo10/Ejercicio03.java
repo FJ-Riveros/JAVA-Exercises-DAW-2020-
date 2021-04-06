a5a6f7326c543f8beca3fca1df01483f26fcea02
FJ-Riveros <fran.etnies@gmail.com>
2021-04-06 11:18:35 +0200

    actualizacion

--- Capitulo10/Ejercicio03.java
+++ Capitulo10/Ejercicio03.java
@@ -8,12 +8,17 @@ import java.util.Collections;
  
 public class Ejercicio03{
   public static void main(String[] args){
+    //Menor a mayor
     ArrayList<Integer> input = new ArrayList<Integer>();
     for(int i=0; i<6; i++){
-      System.out.print("Introduzca un número, le quedan " + (5-i) + " números por introducir: ");
+      System.out.print("Introduzca un número, le quedan " + (6-i) + " números por introducir: ");
       input.add(Integer.parseInt(System.console().readLine()));
     }
     Collections.sort(input);
-    for(int item: input) System.out.println(item);
+    //for(int item: input) System.out.println(item);
+    System.out.println(input);
+    //Mayor a menor
+    Collections.sort(input, Collections.reverseOrder());
+    System.out.println(input);
   }
 }
