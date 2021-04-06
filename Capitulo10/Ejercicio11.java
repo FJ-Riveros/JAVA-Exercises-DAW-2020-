f611d7f1f2d70fb95626c7d7905a825176ef4185
FJ-Riveros <fran.etnies@gmail.com>
2021-04-06 15:05:09 +0200

    actualizacion

--- Capitulo10/Ejercicio11.java
+++ Capitulo10/Ejercicio11.java
@@ -19,19 +19,29 @@ public class Ejercicio11{
     diccionario.put("luz","light");
     diccionario.put("oscuro","dark");
     String[] keys = diccionario.keySet().toArray(new String[0]);
-    
     ArrayList<Integer> nums = new ArrayList<Integer>();
-    nums.add((int)(Math.random()*10));
+    nums.add((int)(Math.random()*9));
     int random = 0;
     for(int i=0; i<4; i++){
       do{
-        random = (int)(Math.random()*10);
+        random = (int)(Math.random()*9);
       }while(nums.contains(random));
+      nums.add(random);
     }
-    
     System.out.println("Traduzca las siguientes palabras: ");
+    int correctas = 0;
+    int incorrectas = 0;
+    String userInput = "";
     for(int i=0; i<nums.size(); i++){
-      System.out.println(keys[nums.get(i)]);
+      System.out.print(keys[nums.get(i)] + ": ");
+      userInput = System.console().readLine();
+      if(diccionario.get(keys[nums.get(i)]).equals(userInput)){
+        correctas++;
+      }else{
+        incorrectas++;
+      }
     }
+    System.out.println("Correctas: " + correctas);
+    System.out.println("Incorrectas: " + incorrectas);
   }
 }
